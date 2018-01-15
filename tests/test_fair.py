18f481970e88bc669d223bd210ff32ba8bc22138
Chris Smith <chrisroadmap@gmail.com>
2018-01-15 09:49:18 +0000

    Add tests for RCP scenarios

--- tests/test_fair.py
+++ tests/test_fair.py
@@ -3,7 +3,7 @@ import pytest
 import fair
 import os
 import numpy as np
-from fair.RCPs import rcp3pd
+from fair.RCPs import rcp3pd, rcp45, rcp6, rcp85
 
 
 def test_import():
@@ -44,6 +44,63 @@ def test_ten_GtC_pulse():
     assert np.allclose(T, T_expected)
 
 
-def test_rcp3pd_multigas_error():
+def test_multigas_fullemissions_error():
     with pytest.raises(ValueError):
         fair.forward.fair_scm(emissions=rcp3pd.Emissions.emissions)
+
+
+# There must be a good way to avoid duplication here
+def test_rcp3pd():
+    C,F,T = fair.forward.fair_scm(emissions=rcp3pd.Emissions.emissions,
+        useMultigas=True)
+
+    datadir = os.path.join(os.path.dirname(__file__), 'rcp3pd/')
+    C_expected = np.load(datadir + 'C.npy')
+    F_expected = np.load(datadir + 'F.npy')
+    T_expected = np.load(datadir + 'T.npy')
+
+    assert np.allclose(C, C_expected)
+    assert np.allclose(F, F_expected)
+    assert np.allclose(T, T_expected)
+
+
+def test_rcp45():
+    C,F,T = fair.forward.fair_scm(emissions=rcp45.Emissions.emissions,
+        useMultigas=True)
+
+    datadir = os.path.join(os.path.dirname(__file__), 'rcp45/')
+    C_expected = np.load(datadir + 'C.npy')
+    F_expected = np.load(datadir + 'F.npy')
+    T_expected = np.load(datadir + 'T.npy')
+
+    assert np.allclose(C, C_expected)
+    assert np.allclose(F, F_expected)
+    assert np.allclose(T, T_expected)
+
+
+def test_rcp6():
+    C,F,T = fair.forward.fair_scm(emissions=rcp6.Emissions.emissions,
+        useMultigas=True)
+
+    datadir = os.path.join(os.path.dirname(__file__), 'rcp6/')
+    C_expected = np.load(datadir + 'C.npy')
+    F_expected = np.load(datadir + 'F.npy')
+    T_expected = np.load(datadir + 'T.npy')
+
+    assert np.allclose(C, C_expected)
+    assert np.allclose(F, F_expected)
+    assert np.allclose(T, T_expected)
+
+
+def test_rcp85():
+    C,F,T = fair.forward.fair_scm(emissions=rcp85.Emissions.emissions,
+        useMultigas=True)
+
+    datadir = os.path.join(os.path.dirname(__file__), 'rcp85/')
+    C_expected = np.load(datadir + 'C.npy')
+    F_expected = np.load(datadir + 'F.npy')
+    T_expected = np.load(datadir + 'T.npy')
+
+    assert np.allclose(C, C_expected)
+    assert np.allclose(F, F_expected)
+    assert np.allclose(T, T_expected)
