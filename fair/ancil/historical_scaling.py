cbf89c8e246e30f877c8d93509c172a559434f80
Chris Smith <chrisroadmap@gmail.com>
2018-03-14 12:31:02 +0000

    Added option and ancil file to get pre-loaded historical scalings on top of user-specified ones

---
+++ fair/ancil/historical_scaling.py
@@ -0,0 +1,9 @@
+# usage
+# from fair.ancil import historical_scaling
+# s = historical_scaling.all
+
+import numpy as np
+import os
+
+filename = os.path.join(os.path.dirname(__file__), 'historical_scaling.csv')
+all = np.loadtxt(filename, skiprows=0, delimiter=',')
