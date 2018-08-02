1973db867118308daf80eeee80ee167e61f81fe5
Chris Smith <chrisroadmap@gmail.com>
2018-08-02 15:00:22 +0100

    Prefix module names

--- fair/RCPs/rcp3pd.py
+++ fair/RCPs/rcp3pd.py
@@ -1,5 +1,5 @@
 import warnings
-from rcp26 import *
+from .rcp26 import *
 
 warnings.warn('name rcp3pd will be deprecated in FaIR 2.0. Please use rcp26.',
   DeprecationWarning)
--- fair/RCPs/rcp6.py
+++ fair/RCPs/rcp6.py
@@ -1,5 +1,5 @@
 import warnings
-from rcp60 import *
+from .rcp60 import *
 
 warnings.warn('name rcp6 will be deprecated in FaIR 2.0. Please use rcp60.',
   DeprecationWarning)
