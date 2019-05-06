042d8e51be64fb08226543f5cd57225d2eb72d53
Chris Smith <chrisroadmap@gmail.com>
2019-05-06 21:05:57 +0100

    Remove header strings

--- fair/__init__.py
+++ fair/__init__.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: zebedeen
-# @Date:   2017-04-10 10:41:20
-# @Last Modified by:   Chris Smith
-# @Last Modified time: 2019-05-03 00:36:00
-
-
-# # # ------------ IMPORT REQUIRED MODULES ------------ # # #
 from . import forward, inverse
 from ._version import get_versions
 __version__ = get_versions()['version']
