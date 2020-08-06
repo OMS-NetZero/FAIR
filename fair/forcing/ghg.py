6a5e0b0f8fe04bc4bc2cd9a77dd1d85d1a22a3b8
Chris Smith <chrisroadmap@gmail.com>
2020-08-06 16:50:45 +0100

    add Meinshausen forcing option for CO2 CH4 N2O

--- fair/forcing/ghg.py
+++ fair/forcing/ghg.py
@@ -4,6 +4,60 @@ import numpy as np
 
 from ..constants import radeff
 
+
+def meinshausen(
+    C,
+    Cpi=np.array([277.15, 731.41, 273.87]),
+    a1=-2.4785e-07, b1=0.00075906, c1=-0.0021492, d1 = 5.2488,
+    a2=-0.00034197, b2 = 0.00025455, c2 =-0.00024357, d2 = 0.12173,
+    a3 =-8.9603e-05, b3 = -0.00012462, d3 = 0.045194,
+    F2x=3.71, scale_F2x=True
+    ):
+    """Modified Etminan relationship from Meinshausen et al 2019
+    https://gmd.copernicus.org/preprints/gmd-2019-222/gmd-2019-222.pdf
+    table 3
+
+    Inputs:
+        C: [CO2, CH4, N2O] concentrations, [ppm, ppb, ppb]
+
+    Keywords:
+        Cpi: pre-industrial [CO2, CH4, N2O] concentrations. Should use defaults
+        a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, d3: coefficients
+
+    Returns:
+        3-element array of radiative forcing: [F_CO2, F_CH4, F_N2O]
+
+    """
+    # Tune the coefficient of CO2 forcing to acheive desired F2x, using
+    # pre-industrial CO2 and N2O. F2x_etminan ~= 3.801.
+    scaleCO2 = 1
+    if scale_F2x:
+        F2x_etminan = (
+          -2.4e-7*Cpi[0]**2 + 7.2e-4*Cpi[0] - 2.1e-4*Cpi[2] + 5.36) * np.log(2)
+        scaleCO2 = F2x/F2x_etminan
+
+    F = np.zeros(3)
+
+    # CO2
+    Camax = Cpi[0] - b1/(2*a1)
+    if Cpi[0] < C[0] <= Camax: # the most likely case
+        alphap = d1 + a1*(C[0] - Cpi[0])**2 + b1*(C[0] - Cpi[0])
+    elif C[0] <= Cpi[0]:
+        alphap = d1
+    else:
+        alphap = d1 - b1**2-(4*a1)
+    alphaN2O = c1*np.sqrt(C[2])
+    F[0] = (alphap + alphaN2O) * np.log(C[0]/Cpi[0]) * scaleCO2
+
+    # CH4
+    F[1] = (a3*np.sqrt(C[1]) + b3*np.sqrt(C[2]) + d3) * (np.sqrt(C[1]) - np.sqrt(Cpi[1]))
+
+    # N2O
+    F[2] = (a2*np.sqrt(C[0]) + b2*np.sqrt(C[2]) + c2*np.sqrt(C[1])) * (np.sqrt(C[2]) - np.sqrt(Cpi[2]))
+
+    return F
+
+
 def etminan(C, Cpi, F2x=3.71, scale_F2x=True):
     """Calculate the radiative forcing from CO2, CH4 and N2O.
 
