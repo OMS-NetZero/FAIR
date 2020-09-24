6f482ad04bfbf34ba95062c07a52328123749900
John Broadbent <johngeoffreybroadbent@gmail.com>
2020-09-24 16:13:16 +0100

    Reformated cfg
    cfg dict reformatted to use pd.dataframes for gas, thermal params etc. Also built in support for aerosol emissions, through putting aer_conc into gas_params.

--- fair/2.0.0/concentration_driven.py
+++ fair/2.0.0/concentration_driven.py
@@ -12,7 +12,7 @@ def run(inp_df, cfg):
         Input :obj:`pd.DataFrame` containing the timeseries to run
 
     cfg : dict
-        Dictionary containing the configuration for this run
+        Dictionary containing the configuration for this run, in format {'gas_params' : :obj:`pd.DataFrame`, 'thermal_params': :obj:`pd.DataFrame`, 'ext_forcing' : :obj:`pd.DataFrame`}
 
     Returns
     -------
@@ -21,30 +21,74 @@ def run(inp_df, cfg):
     """
     raise NotImplementedError
     np_input = unifiedtools.convert_df_to_numpy(inp_df)
-    res_dict = _run_numpy(  np_input,\
-                            a1 = cfg['a1'],\
-                            a2 = cfg['a2'],\
-                            a3 = cfg['a3'],\
-                            a4 = cfg['a4'],\
-                            tau1 = cfg['tau1'],\
-                            tau2 = cfg['tau2'],\
-                            tau3 = cfg['tau3'],\
-                            tau4 = cfg['tau4'],\
-                            r0 = cfg['r0'],\
-                            rC = cfg['rC'],\
-                            rT = cfg['rT'],\
-                            rA = cfg['rA'],\
-                            PI_conc = cfg['PI_conc'],\
-                            emis2conc = cfg['emis2conc'],\
-                            f1 = cfg['f1'],\
-                            f2 = cfg['f2'],\
-                            f3 = cfg['f3'],\
-                            d = cfg['d'],\
-                            q = cfg['q'],\
-                            ext_forcing = cfg['ext_forcing'],\
-                            timestep = cfg['timestep'])
+    df_gas_params = cfg['gas_params']
+    df_thermal_params = cfg['thermal_params']
+    df_ext_forcing = cfg['ext_forcing']
+
+    #np_gas_params is in order [[parameter], [gas]], parameters in alphabetical order: a1, a2, a3, a4, aer_conc, emis2conc, f1, f2, f3, PI_conc, r0, rA, rC, rT, tau1, tau2, tau3, tau4
+    np_gas_params = unifiedtools.convert_df_to_numpy(df_gas_params)
+    np_a1 = np_gas_params[0,:]
+    np_a2 = np_gas_params[1,:]
+    np_a3 = np_gas_params[2,:]
+    np_a4 = np_gas_params[3,:]
+    np_aer_conc = np_gas_params[4,:]
+    np_emis2conc = np_gas_params[5,:]
+    np_f1 = np_gas_params[6,:]
+    np_f2 = np_gas_params[7,:]
+    np_f3 = np_gas_params[8,:]
+    np_PI_conc = np_gas_params[9,:]
+    np_r0 = np_gas_params[10,:]
+    np_rA = np_gas_params[11,:]
+    np_rC = np_gas_params[12,:]
+    np_rT = np_gas_params[13,:]
+    np_tau1 = np_gas_params[14,:]
+    np_tau2 = np_gas_params[15,:]
+    np_tau3 = np_gas_params[16,:]
+    np_tau4 = np_gas_params[17,:]
+    #np_thermal_params is in order [[parameter], [thermal box]] with parameters in alphabetical order: d, q
+    np_thermal_params = unifiedtools.convert_df_to_numpy(df_thermal_params)
+    np_d = np_thermal_params[0,:]
+    np_q = np_thermal_params[1,:]
+
+    np_ext_forcing = unifiedtools.convert_df_to_numpy(df_ext_forcing)
+
     inp_df.columns.values.sort()
     inp_df.index.values.sort()
+    df_gas_params.columns.values.sort()
+    df_gas_params.index.values.sort()
+    df_thermal_params.columns.values.sort()
+    df_thermal_params.index.values.sort()
+    time_index = inp_df.index
+
+    np_timestep = np.append(np.diff(time_index),np.diff(time_index)[-1])
+    
+
+    #Turns aerosol emissions into concentrations, by adding in Pre-Industrial Concentration
+    np_concentrations = np_input + np_PI_conc*np_aer_conc
+
+    res_dict = _run_numpy(  np_concentrations,\
+                            a1 = np_a1,\
+                            a2 = np_a2,\
+                            a3 = np_a3,\
+                            a4 = np_a4,\
+                            tau1 = np_tau1,\
+                            tau2 = np_tau2,\
+                            tau3 = np_tau3,\
+                            tau4 = np_tau4,\
+                            r0 = np_r0,\
+                            rC = np_rC,\
+                            rT = np_rT,\
+                            rA = np_rA,\
+                            PI_conc = np_PI_conc,\
+                            emis2conc = np_emis2conc,\
+                            f1 = np_f1,\
+                            f2 = np_f2,\
+                            f3 = np_f3,\
+                            d = np_d,\
+                            q = np_q,\
+                            ext_forcing = np_ext_forcing,\
+                            timestep = np_timestep)
+    
     emissions_array = res_dict["C"]
     RF_array = res_dict["RF"]
     T_array = res_dict["T"]
@@ -71,7 +115,7 @@ def run(inp_df, cfg):
                                                         inp_df.columns.name,\
                                                         inp_df.index.values,\
                                                         inp_df.index.name)
-    res_df_dict = {'emissions':emissions_df, 'C':inp_df, 'RF' : RF_df, 'T' : T_df, 'alpha':alpha_df}
+    res_df_dict = {'emissions':emissions_df, 'C':inp_df, 'RF' : RF_df, 'T' : T_df, 'alpha':alpha_df, 'gas_params':df_gas_params, 'thermal_params':df_thermal_params}
     return res_df_dict
 
 def _run_numpy( inp_ar,\
