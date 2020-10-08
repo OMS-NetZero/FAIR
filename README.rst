12d32daa71fde786c3e77f96f2380fddc45bcb9a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-10-08 21:51:49 +1100

    Setup for developer use

--- README.rst
+++ README.rst
@@ -3,4 +3,8 @@ FaIR
 
 Finite Amplitude Impulse-Response simple climate-carbon-cycle model
 
+.. sec-begin-long-description
+
 [TODO re-write this]
+
+.. sec-end-long-description
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
 import versioneer
 
-# README #
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+PACKAGE_NAME = "fair"
+DESCRIPTION = "Data handling for simple climate model data"
+KEYWORDS = ['simple climate model', 'temperature response', 'carbon cycle', 'emissions', 'forcing']
 
 AUTHORS = [
     ("John Broadbent", "johngeoffreybroadbent@gmail.com"),
@@ -14,32 +14,108 @@ AUTHORS = [
     ("Zeb Nicholls", "zebedee.nicholls@climate-energy-college.org"),
 ]
 
-setup(
-    name='fair',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description='Python package to perform calculations with the FaIR simple climate model',
-    long_description=readme(),
-    keywords='simple climate model temperature response carbon cycle emissions forcing',
-    url='https://github.com/OMS-NetZero/FAIR',
-    author=", ".join([author[0] for author in AUTHORS]),
-    author_email=", ".join([author[1] for author in AUTHORS]),
-    license='Apache 2.0',
-    packages=find_packages(exclude=['tests*','docs*']),
-    package_data={'': ['*.csv']},
-    python_requires='>=3.6, <4',
-    include_package_data=True,
-    install_requires=[
+URL = "https://github.com/OMS-NetZero/FAIR"
+PROJECT_URLS = {
+    "Bug Reports": "https://github.com/OMS-NetZero/FAIR/issues",
+    "Source": "https://github.com/OMS-NetZero/FAIR",
+}
+LICENSE = "Apache 2.0"
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: Apache License",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+]
+
+PYTHON_REQUIREMENTS = '>=3.6, <4'
+REQUIREMENTS = [
         'matplotlib',
         'numexpr',
         'numpy>=1.14.5',
         'pandas',
         'scipy>=0.19.0',
+    ]
+REQUIREMENTS_TESTS = (
+    ['pytest>=4.0', 'nbval', 'pytest-cov', 'codecov']
+)
+REQUIREMENTS_DEPLOY = ["twine>=1.11.0", "setuptools>=41.2", "wheel>=0.31.0"]
+
+REQUIREMENTS_DEV = [
+    *[
+        "black==19.10b0",
+        "flake8",
+        "isort>=5",
     ],
+    *REQUIREMENTS_TESTS,
+    *REQUIREMENTS_DEPLOY,
+]
+
+REQUIREMENTS_EXTRAS = {
+    "tests": REQUIREMENTS_TESTS,
+    "deploy": REQUIREMENTS_DEPLOY,
+    "dev": REQUIREMENTS_DEV,
+}
+
+SOURCE_DIR = "fair"
+
+PACKAGES = find_packages(exclude=['tests*','docs*'])
+PACKAGE_DATA = {"": ["*.csv"]}
+
+README = "README.rst"
+
+# Get the long description from the README file
+with open(README, "r") as f:
+    README_LINES = ["FaIR", "====", ""]
+    add_line = False
+    for line in f:
+        if line.strip() == ".. sec-begin-long-description":
+            add_line = True
+        elif line.strip() == ".. sec-end-long-description":
+            break
+        elif add_line:
+            README_LINES.append(line.strip())
+
+if len(README_LINES) < 3:
+    raise RuntimeError("Insufficient description given")
+
+
+class Fair(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        pytest.main(self.test_args)
+
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass.update({"test": Fair})
+
+setup(
+    name=PACKAGE_NAME,
+    version=versioneer.get_version(),
+    cmdclass=cmdclass,
+    description=DESCRIPTION,
+    long_description="\n".join(README_LINES),
+    long_description_content_type="text/x-rst",
+    author=", ".join([author[0] for author in AUTHORS]),
+    author_email=", ".join([author[1] for author in AUTHORS]),
+    url=URL,
+    project_urls=PROJECT_URLS,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    extras_require=REQUIREMENTS_EXTRAS,
+    python_requires=PYTHON_REQUIREMENTS,
     zip_safe=False,
-    extras_require={
-        'docs': ['sphinx>=1.4', 'nbsphinx'],
-        'dev' : ['notebook', 'wheel', 'twine'],
-        'tests': ['pytest>=4.0', 'nbval', 'pytest-cov', 'codecov']
-    }
 )
