869f22fac39105a359ab537e77445e66ec25fa0b
Chris Smith <chrisroadmap@gmail.com>
2021-09-10 10:00:44 +0100

    add NOX and CH4 fractions for some SSPs

--- fair/SSPs/__init__.py
+++ fair/SSPs/__init__.py
@@ -38,28 +38,45 @@ class ssp370:
     Emissions = load_emissions_data(emissions_filename, "ssp370")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp370")
     Forcing = load_forcing_data(forcing_filename, "ssp370")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
+    
 
 class ssp370_lowNTCF:
     Emissions = load_emissions_data(emissions_filename, "ssp370-lowNTCF-aerchemmip")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp370-lowNTCF-aerchemmip")
     Forcing = load_forcing_data(forcing_filename, "ssp370-lowNTCF-aerchemmip")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
+
     
 class ssp434:
     Emissions = load_emissions_data(emissions_filename, "ssp434")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp434")
     Forcing = load_forcing_data(forcing_filename, "ssp434")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
+    
 
 class ssp460:
     Emissions = load_emissions_data(emissions_filename, "ssp460")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp460")
     Forcing = load_forcing_data(forcing_filename, "ssp460")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
 
+    
 class ssp534:
     Emissions = load_emissions_data(emissions_filename, "ssp534-over")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp534-over")
     Forcing = load_forcing_data(forcing_filename, "ssp534-over")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
 
+    
 class ssp585:
     Emissions = load_emissions_data(emissions_filename, "ssp585")
     Concentrations = load_concentrations_data(concentrations_filename, "ssp585")
     Forcing = load_forcing_data(forcing_filename, "ssp585")
+    fossilCH4_frac = Emissions.fossilCH4_frac
+    aviNOx_frac = Emissions.aviNOx_frac
