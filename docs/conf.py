b22fc3bb8c8b4a6694f94e77ff5362104fde427b
Chris Smith <chrisroadmap@gmail.com>
2022-12-13 15:26:45 -0600

    update index

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-root_doc = 'i2'
+root_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
