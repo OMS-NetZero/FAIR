3ff411406f63660c4d9f813d32026fe576cb8904
Chris Smith <chrisroadmap@gmail.com>
2024-10-30 10:11:21 +0000

    add support for 3.13

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     keywords="simple, climate, model, temperature, forcing, emissions, emulator",
     package_dir={"": "src"},
